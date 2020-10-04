e660ad4353a0b86c7b151e26254c9c278cb8f198
Matheus Alano <matheus_dias@terceiros.sicredi.com.br>
2020-10-04 20:38:13 -0300

    feat(filter): add viewModel layer

--- MealApp/Scenes/Filter/MAFilterViewModel.swift
+++ MealApp/Scenes/Filter/MAFilterViewModel.swift
@@ -2,11 +2,18 @@ import Foundation
 import RxCocoa
 import RxSwift
 
+enum MAFilterViewModelState: Equatable {
+    case loading, error(_ error: String), ingredients, area
+}
+
 protocol MAFilterViewModelProtocol {
     typealias Target = MAFilterCoordinator.Target
 
     var didTapLeftBarButton: PublishSubject<Void> { get }
+    var didSelectCell: PublishSubject<IndexPath> { get }
 
+    var state: Driver<MAFilterViewModelState> { get }
+    var dataSource: Driver<[MAFilterSectionModel]> { get }
     var navigationTarget: Driver<Target> { get }
 }
 
@@ -14,14 +21,129 @@ final class MAFilterViewModel: MAFilterViewModelProtocol {
     //MARK: Inputs
 
     let didTapLeftBarButton = PublishSubject<Void>()
+    let didSelectCell = PublishSubject<IndexPath>()
 
     //MARK: Outputs
 
+    let state: Driver<MAFilterViewModelState>
+    let dataSource: Driver<[MAFilterSectionModel]>
     let navigationTarget: Driver<Target>
 
-    init() {
+    init(service: MAFilterServiceProtocol = MAFilterService()) {
+        let _state = PublishSubject<MAFilterViewModelState>()
+        state = _state.asDriver(onErrorDriveWith: .never())
+
+        let selectedFilter = didSelectCell
+            .filter({ $0.section == 0 })
+            .compactMap({ FilterOption(rawValue: $0.row) })
+            .distinctUntilChanged()
+
+        //MARK: Ingredients
+
+        let ingredientsData = selectedFilter
+            .filter({ $0 == .ingredients })
+            .takeUntil(_state.filter({ $0 == .ingredients }))
+            .flatMapLatest({ _ in
+                service.getIngredients()
+                    .asObservable()
+                    .do(onNext: { _ in _state.onNext(.ingredients) }, onSubscribe: { _state.onNext(.loading) })
+                    .catchError {
+                        let error: ServiceError = $0 as? ServiceError ?? .generic
+                        _state.onNext(.error(error.readableMessage))
+                        return .never()
+                    }
+                    .map({ $0.list })
+                    .startWith([])
+            })
+            .map({ MAFilterSectionModel.optionSection(items: $0.map({ item in .option(item.value) })) })
+            .share(replay: 1)
+
+        let ingredients = Observable.merge(
+            selectedFilter.filter({ $0 == .ingredients }).withLatestFrom(ingredientsData),
+            ingredientsData
+        ).map({ (FilterOption.ingredients, $0) })
+
+        //MARK: Areas
+
+        let areasData = selectedFilter
+            .filter({ $0 == .area })
+            .takeUntil(_state.filter({ $0 == .area }))
+            .flatMapLatest({ _ in
+                service.getAreas()
+                    .asObservable()
+                    .do(onNext: { _ in _state.onNext(.area) }, onSubscribe: { _state.onNext(.loading) })
+                    .catchError {
+                        let error: ServiceError = $0 as? ServiceError ?? .generic
+                        _state.onNext(.error(error.readableMessage))
+                        return .never()
+                    }
+                    .map({ $0.list })
+                    .startWith([])
+            })
+            .map({ MAFilterSectionModel.optionSection(items: $0.map({ item in .option(item.value) })) })
+            .share(replay: 1)
+
+        let areas = Observable.merge(
+            selectedFilter.filter({ $0 == .area }).withLatestFrom(areasData),
+            areasData
+        ).map({ (FilterOption.area, $0) })
+
+        //MARK: Datasource
+
+        let options = Observable.merge(areas, ingredients)
+            .asDriver(onErrorDriveWith: .never())
+            .startWith((.none, .optionSection(items: [])))
+
+        dataSource = options
+            .map({ filterOption, options in
+                var sections: [MAFilterSectionModel] = [
+                    .filterSection(items: [.filterOption(.init(title: "By Area", systemImage: "mappin.circle.fill", selected: filterOption == .area)),
+                                           .filterOption(.init(title: "By main ingredient", systemImage: "i.circle.fill", selected: filterOption == .ingredients))])
+                ]
+
+                sections.append(options)
+                return sections
+            })
+            .distinctUntilChanged()
+
+        //MARK: Target
+
+        let selectedOption = didSelectCell
+            .filter({ $0.section == 1 })
+            .withLatestFrom(options, resultSelector: { indexPath, data in (data.0, data.1.items[indexPath.row]) })
+
+        let selectedArea = selectedOption
+            .flatMapLatest({ selected -> Observable<String> in
+                switch selected {
+                case let (.area, .option(value)):
+                    return .just(value)
+                default:
+                    return .never()
+                }
+            })
+
+        let selectedIngredient = selectedOption
+            .flatMapLatest({ selected -> Observable<String> in
+                switch selected {
+                case let (.ingredients, .option(value)):
+                    return .just(value)
+                default:
+                    return .never()
+                }
+            })
+
         navigationTarget = Observable.merge(
-            didTapLeftBarButton.map({ .settings })
+            didTapLeftBarButton.map({ .settings }),
+            selectedArea.map({ .mealsFromArea(area: $0) }),
+            selectedIngredient.map({ .mealsWithIngredient(ingredient: $0) })
         ).asDriver(onErrorRecover: { _ in .empty() })
+    }
+}
+
+extension MAFilterViewModel {
+    private enum FilterOption: Int {
+        case area = 0
+        case ingredients = 1
+        case none = 2
     }
 }
--- MealAppTests/Scenes/Filter/MAFilterViewModelTests.swift
+++ MealAppTests/Scenes/Filter/MAFilterViewModelTests.swift
@@ -6,6 +6,7 @@ import RxTest
 @testable import MealApp
 
 final class MAFilterViewModelTests: QuickSpec {
+    private var service: MAFilterServiceMock!
     private var sut: MAFilterViewModel!
     private var scheduler: TestScheduler!
     private var disposeBag: DisposeBag!
@@ -16,15 +17,216 @@ final class MAFilterViewModelTests: QuickSpec {
         start()
     }
 
-    private func setup() {
+    private func setup(areasResult: ResultType = .success, ingredientsResult: ResultType = .success) {
         scheduler = TestScheduler(initialClock: 0)
         disposeBag = DisposeBag()
 
-        sut = MAFilterViewModel()
+        service = MAFilterServiceMock(areasResult: areasResult, ingredientsResult: ingredientsResult)
+        sut = MAFilterViewModel(service: service)
     }
 
     private func start() {
         describe("MAFilterViewModel") {
+            when("when initialized") {
+                beforeEach {
+                    self.setup()
+                }
+
+                then("then datasource must be correct") {
+                    let truth = MAFilterSectionModel.filterSection(
+                        items: [.filterOption(.init(title: "By Area", systemImage: "mappin.circle.fill", selected: false)),
+                                .filterOption(.init(title: "By main ingredient", systemImage: "i.circle.fill", selected: false))])
+                    let observer = self.scheduler.start({ self.sut.dataSource.asObservable() })
+                    expect(observer.events).to(equal([.next(200, [truth, .optionSection(items: [])])]))
+                }
+            }
+
+            when("when area filter is selected") {
+                beforeEach {
+                    self.setup()
+
+                    self.scheduler.createHotObservable([.next(300, IndexPath(row: 0, section: 0))])
+                        .bind(to: self.sut.didSelectCell)
+                        .disposed(by: self.disposeBag)
+                    self.sut.dataSource.drive().disposed(by: self.disposeBag)
+                }
+
+                context("twice") {
+                    beforeEach {
+                        self.scheduler.createHotObservable([.next(350, IndexPath(row: 1, section: 0)), .next(400, IndexPath(row: 0, section: 0))])
+                            .bind(to: self.sut.didSelectCell)
+                            .disposed(by: self.disposeBag)
+                    }
+
+                    then("then service must be called once") {
+                        let observer = self.scheduler.start({ self.service.areasCalled.map({ _ in true }) })
+                        expect(observer.events).to(equal([.next(300, true)]))
+                    }
+                }
+
+                then("then service must be called") {
+                    let observer = self.scheduler.start({ self.service.areasCalled.map({ _ in true }) })
+                    expect(observer.events).to(equal([.next(300, true)]))
+                }
+
+                then("then loader must be triggered") {
+                    let observer = self.scheduler.start({ self.sut.state.asObservable() })
+                    expect(observer.events).to(contain([.next(300, .loading)]))
+                }
+            }
+
+            when("when ingredient filter is selected") {
+                beforeEach {
+                    self.setup()
+
+                    self.scheduler.createHotObservable([.next(300, IndexPath(row: 1, section: 0))])
+                        .bind(to: self.sut.didSelectCell)
+                        .disposed(by: self.disposeBag)
+                    self.sut.dataSource.drive().disposed(by: self.disposeBag)
+                }
+
+                context("twice") {
+                    beforeEach {
+                        self.scheduler.createHotObservable([.next(350, IndexPath(row: 0, section: 0)), .next(400, IndexPath(row: 1, section: 0))])
+                            .bind(to: self.sut.didSelectCell)
+                            .disposed(by: self.disposeBag)
+                    }
+
+                    then("then service must be called once") {
+                        let observer = self.scheduler.start({ self.service.ingredientsCalled.map({ _ in true }) })
+                        expect(observer.events).to(equal([.next(300, true)]))
+                    }
+                }
+
+                then("then service must be called") {
+                    let observer = self.scheduler.start({ self.service.ingredientsCalled.map({ _ in true }) })
+                    expect(observer.events).to(equal([.next(300, true)]))
+                }
+
+                then("then loader must be triggered") {
+                    let observer = self.scheduler.start({ self.sut.state.asObservable() })
+                    expect(observer.events).to(contain([.next(300, .loading)]))
+                }
+            }
+
+            given("given generic request error") {
+                beforeEach {
+                    self.setup(areasResult: .failure(error: .generic), ingredientsResult: .failure(error: .generic))
+                }
+
+                when("when area service return") {
+                    beforeEach {
+                        self.scheduler.createHotObservable([.next(300, IndexPath(row: 0, section: 0))])
+                            .bind(to: self.sut.didSelectCell)
+                            .disposed(by: self.disposeBag)
+                        self.sut.dataSource.drive().disposed(by: self.disposeBag)
+                    }
+
+                    then("then error must be presented") {
+                        let observer = self.scheduler.start({ self.sut.state.asObservable() })
+                        expect(observer.events).to(contain([.next(300, .loading), .next(300, .error(.localized(by: MAString.Errors.generic)))]))
+                    }
+                }
+
+                when("when ingredient service return") {
+                    beforeEach {
+                        self.scheduler.createHotObservable([.next(300, IndexPath(row: 1, section: 0))])
+                            .bind(to: self.sut.didSelectCell)
+                            .disposed(by: self.disposeBag)
+                        self.sut.dataSource.drive().disposed(by: self.disposeBag)
+                    }
+
+                    then("then error must be presented") {
+                        let observer = self.scheduler.start({ self.sut.state.asObservable() })
+                        expect(observer.events).to(contain([.next(300, .loading), .next(300, .error(.localized(by: MAString.Errors.generic)))]))
+                    }
+                }
+            }
+
+            when("when area service return") {
+                beforeEach {
+                    self.setup()
+                    self.scheduler.createHotObservable([.next(300, IndexPath(row: 0, section: 0))])
+                        .bind(to: self.sut.didSelectCell)
+                        .disposed(by: self.disposeBag)
+                }
+
+                then("then datasource must be correct") {
+                    let truth = MAFilterSectionModel.filterSection(
+                        items: [.filterOption(.init(title: "By Area", systemImage: "mappin.circle.fill", selected: true)),
+                                .filterOption(.init(title: "By main ingredient", systemImage: "i.circle.fill", selected: false))])
+
+                    let observer = self.scheduler.start({ self.sut.dataSource.asObservable() })
+                    expect(observer.events).to(
+                        contain([.next(300, [truth, .optionSection(items: [])]),
+                                 .next(300, [truth, .optionSection(items: [.option("Opt1"), .option("Opt2")])])]))
+                }
+            }
+
+            when("when ingredient service return") {
+                beforeEach {
+                    self.setup()
+                    self.scheduler.createHotObservable([.next(300, IndexPath(row: 1, section: 0))])
+                        .bind(to: self.sut.didSelectCell)
+                        .disposed(by: self.disposeBag)
+                }
+
+                then("then datasource must be correct") {
+                    let truth = MAFilterSectionModel.filterSection(
+                        items: [.filterOption(.init(title: "By Area", systemImage: "mappin.circle.fill", selected: false)),
+                                .filterOption(.init(title: "By main ingredient", systemImage: "i.circle.fill", selected: true))])
+
+                    let observer = self.scheduler.start({ self.sut.dataSource.asObservable() })
+                    expect(observer.events).to(
+                        contain([.next(300, [truth, .optionSection(items: [])]),
+                                 .next(300, [truth, .optionSection(items: [.option("Opt1"), .option("Opt2")])])]))
+                }
+            }
+
+            given("given area filter is selected") {
+                beforeEach {
+                    self.setup()
+                    self.scheduler.createHotObservable([.next(300, IndexPath(row: 0, section: 0))])
+                        .bind(to: self.sut.didSelectCell)
+                        .disposed(by: self.disposeBag)
+                }
+
+                when("when a area option is selected") {
+                    beforeEach {
+                        self.scheduler.createHotObservable([.next(400, IndexPath(row: 0, section: 1))])
+                            .bind(to: self.sut.didSelectCell)
+                            .disposed(by: self.disposeBag)
+                    }
+
+                    then("then meal list must be requested") {
+                        let observer = self.scheduler.start({ self.sut.navigationTarget.asObservable() })
+                        expect(observer.events).to(equal([.next(400, .mealsFromArea(area: "Opt1"))]))
+                    }
+                }
+            }
+
+            given("given ingredients filter is selected") {
+                beforeEach {
+                    self.setup()
+                    self.scheduler.createHotObservable([.next(300, IndexPath(row: 1, section: 0))])
+                        .bind(to: self.sut.didSelectCell)
+                        .disposed(by: self.disposeBag)
+                }
+
+                when("when a ingredients option is selected") {
+                    beforeEach {
+                        self.scheduler.createHotObservable([.next(400, IndexPath(row: 1, section: 1))])
+                            .bind(to: self.sut.didSelectCell)
+                            .disposed(by: self.disposeBag)
+                    }
+
+                    then("then meal list must be requested") {
+                        let observer = self.scheduler.start({ self.sut.navigationTarget.asObservable() })
+                        expect(observer.events).to(equal([.next(400, .mealsWithIngredient(ingredient: "Opt2"))]))
+                    }
+                }
+            }
+
             when("when left bar button is pressed") {
                 beforeEach {
                     self.setup()
@@ -39,6 +241,51 @@ final class MAFilterViewModelTests: QuickSpec {
                     expect(observer.events).to(equal([.next(300, .settings)]))
                 }
             }
+        }
+    }
+}
+
+private final class MAFilterServiceMock: MAFilterServiceProtocol {
+    var areasResult: ResultType
+    var areasCalled = PublishSubject<Void>()
+
+    var ingredientsResult: ResultType
+    var ingredientsCalled = PublishSubject<Void>()
+
+    init(areasResult: ResultType, ingredientsResult: ResultType) {
+        self.areasResult = areasResult
+        self.ingredientsResult = ingredientsResult
+    }
+
+    func getAreas() -> Single<MAFilterResponse> {
+        return Single.create { [weak self] (observer) -> Disposable in
+            guard let self = self else { return Disposables.create() }
+
+            self.areasCalled.onNext(())
+            switch self.areasResult {
+            case .success:
+                observer(.success(.dummy))
+            case let .failure(error: error):
+                observer(.error(error))
+            }
+
+            return Disposables.create()
+        }
+    }
+
+    func getIngredients() -> Single<MAFilterResponse> {
+        return Single.create { [weak self] (observer) -> Disposable in
+            guard let self = self else { return Disposables.create() }
+
+            self.ingredientsCalled.onNext(())
+            switch self.ingredientsResult {
+            case .success:
+                observer(.success(.dummy))
+            case let .failure(error: error):
+                observer(.error(error))
+            }
+
+            return Disposables.create()
         }
     }
 }
